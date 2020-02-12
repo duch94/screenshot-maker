2515cdcc60ef3b6aebb252b2bafa9b5b084b7993
duch94 <vtduch@gmail.com>
2020-02-12 09:03:44 +0300

    Huge refactor. Refactored API. Added schema for user-defined actions.

--- main.py
+++ main.py
@@ -1,29 +1,48 @@
 from aiohttp import web
+import asyncio
+import uuid
 
 from screenshot_maker import make_screenshot
 from logger import get_logger
+from typing import Union
+import os
+import json
 
 logger = get_logger(__name__)
+path_to_images = os.path.join("/var", "tmp", "screenshots")
 
 
-async def get_screenshot(request: web.Request):
-    date_start = request.match_info.get("date_start")
-    date_end = request.match_info.get("date_end")
-    logger.info("Received request: date_start=%s, date_end=%s" % (date_start, date_end))
-    url = "https://yandex.ru/search/?text=%s_%s" % (date_start, date_end)
-    css_sel = "html"
-    logger.info("Started taking screenshot for url %s with css_selector='%s'." % (url, css_sel))
-    screenshot = make_screenshot(url, css_sel)
-    return web.Response(body=screenshot)
+async def order_screenshot(request: web.Request) -> web.Response:
+    try:
+        params: dict = await request.json(loads=json.loads)
+    except json.decoder.JSONDecodeError as e:
+        logger.error("While decoding request error occured: user's payload was [%s]" % e)
+        return web.Response(text="Error occured while parsing json.", status=400)
+    actions = params["actions"]
+    url = params["url"]
+    logger.info("Received request: %s" % str(params))
+    logger.info("Started taking screenshot for url %s with actions='%s'." % (url, str(actions)))
+    img_id = str(uuid.uuid4())
+    asyncio.create_task(make_screenshot(img_id, url, actions))
+    return web.Response(text=img_id, status=202)
+
+
+async def download_order(request: web.Request) -> Union[web.FileResponse, web.Response]:
+    img_id = request.match_info.get("img_id")
+    img_filename = f"{img_id}.png"
+    if os.path.exists(os.path.join(path_to_images, f"{img_filename}.SUCCESS")):
+        return web.FileResponse(path=os.path.join(path_to_images, img_filename))
+    return web.Response(status=102, text="Screenshot making is in progress")
 
 
 async def app_factory():
     app = web.Application()
-    app.add_routes([web.get("/get_screenshot/{date_start}_{date_end}.png", get_screenshot)])
+    app.add_routes([web.post("/order_screenshot", order_screenshot),
+                    web.get("/download_order?img_id={img_id}", download_order)])
     logger.info("Application have been started.")
     return app
 
 
 if __name__ == "__main__":
-    app = app_factory()
-    web.run_app(app)
+    app_for_debug = app_factory()
+    web.run_app(app_for_debug)
--- screenshot_maker.py
+++ screenshot_maker.py
@@ -1,13 +1,16 @@
-from typing import AsyncGenerator
+import asyncio
+import os
 
 from pyppeteer import launch
+from pyppeteer.page import Page
 
 from logger import get_logger
 
 logger = get_logger(__name__)
+path_to_images = os.path.join("/var", "tmp", "screenshots")
 
 
-async def make_screenshot(url: str, css_selector: str) -> AsyncGenerator[bytes, None]:
+async def make_screenshot(img_id: str, url: str, actions: list):
     args = {
         "headless": True,
     }
@@ -16,8 +19,32 @@ async def make_screenshot(url: str, css_selector: str) -> AsyncGenerator[bytes, 
     logger.info("Browser have been loaded with args %s." % str(args))
     await page.goto(url)
     logger.info("Page %s have been loaded." % url)
-    object_to_shot = await page.querySelector(css_selector)
-    logger.info("Element with css_selector='%s' have been selected." % css_selector)
-    screenshot = await object_to_shot.screenshot()
-    logger.info("Screenshot have been made.")
-    yield screenshot
+    screenshot_path = os.path.join(path_to_images, f"{img_id}.png")
+    asyncio.create_task(do_actions(actions=actions, page=page, img_path=screenshot_path))
+
+
+async def do_actions(actions: list, page: Page, img_path: str):
+    for a in actions:
+        # === configuring page ===
+        if a["action"] == "set_resolution":
+            await page.setViewport({
+                "width": a["width"],
+                "height": a["height"]
+            })
+        # === actions ===
+        if "selector" in a.keys():
+            element = await page.querySelector(a["selector"])
+            logger.warning("Action have no CSS selector, it contains %s. Skipping..." % str(a))
+        else:
+            continue
+        if a["action"] == "click":
+            await element.click()
+            logger.info("Element at selector=%s have been clicked." % a["selector"])
+        if a["action"] == "write":
+            await element.type(a["text"])
+            logger.info("Text [%s] have been written at selector=%s" % (a["text"], a["selector"]))
+        if a["action"] == "screenshot_element":
+            await element.screenshot(path=img_path)
+            with open(f"{img_path}.SUCCESS", "w") as file_signal:
+                file_signal.write("")
+            logger.info("Screenshot at selector=%s have been made." % a["selector"])
